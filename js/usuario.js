6f5b429553a1cfa1d2886ec04483d8da7cb5f239
Kevin <ksvergell@ufpso.edu.co>
2022-12-12 22:01:15 -0500

    Arreglos conexion

--- js/usuario.js
+++ js/usuario.js
@@ -671,3 +671,325 @@ function verArticulo(codigo){
             myModal.toggle();
     })
 }
+//-----------------------------------Categorias
+
+
+function listarCategorias(){
+    validaToken();
+    var settings={
+        method: 'GET',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+    }
+    fetch(urlApi+"/categorias",settings)
+    .then(response => response.json())
+    .then(function(data){
+
+        var categorias = `
+            <div class="p-3 mb-2 bg-light text-dark">
+                    <h1 class="display-5"><i class="fa-solid fa-list"></i> Listado de Categorias</h1>
+                </div>
+                  
+                <a href="#" onclick="registerFormCategoria('true')" class="btn btn-outline-success"><i class="fa-solid fa-user-plus"></i></a>
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Descripcion</th>
+
+                        </tr>
+                    </thead>
+                    <tbody id="listar">`;
+            for(const categoria of data){
+                console.log(categoria.id)
+                categorias += `
+                
+                        <tr>
+
+                            <th scope="row">${categoria.id}</th>
+                            <th>${categoria.nombre}</th>
+                            <th>${categoria.descripcion}</th>
+                           
+
+                            
+                            <td>
+                            <button type="button" class="btn btn-outline-danger" 
+                            onclick="eliminaCategoria('${categoria.id}')">
+                                <i class="fa-solid fa-user-minus"></i>
+                            </button>
+                            <a href="#" onclick="verModificarCategoria('${categoria.id}')" class="btn btn-outline-warning">
+                                <i class="fa-solid fa-user-pen"></i>
+                            </a>
+                            <a href="#" onclick="verCategoria('${categoria.id}')" class="btn btn-outline-info">
+                                <i class="fa-solid fa-eye"></i>
+                            </a>
+                            </td>
+                        </tr>
+                    `;
+                
+            }
+            categorias += `
+            </tbody>
+                </table>
+            `;
+            document.getElementById("datos").innerHTML = categorias;
+
+    })
+}
+function registerFormCategoria(auth=false){
+    cadena = `
+            <div class="p-3 mb-2 bg-light text-dark">
+                <h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Registrar Categoria</h1>
+            </div>
+              
+            <form action="" method="post" id="myFormReg">
+                <input type="hidden" name="id" id="id">
+                <label for="nombre" class="form-label">Nombre</label>
+                <input type="text" class="form-control" name="nombre" id="nombre" required> <br>
+
+                <label for="descripcion"  class="form-label">Descripcion</label>
+                <input type="text" class="form-control" name="descripcion" id="descripcion" required> <br>
+                
+
+                <button type="button" class="btn btn-outline-info" onclick="registrarCategoria('${auth}')">Registrar</button>
+            </form>`;
+            document.getElementById("registro").innerHTML = cadena;
+            var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
+            myModal.toggle();
+}
+async function registrarCategoria(auth=false){
+    validaToken();
+    var myForm = document.getElementById("myFormReg");
+    var formData = new FormData(myForm);
+    var jsonData = {};
+    for(var [k, v] of formData){//convertimos los datos a json
+        jsonData[k] = v;
+    }
+    var categoria = jsonData.categoria
+    jsonData.categoria = {
+        id: categoria 
+    }
+    console.log("data user ",jsonData);
+
+    const request = await fetch(urlApi+"/categoria", {
+        method: 'POST',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+        body: JSON.stringify(jsonData)
+    })
+    .then(response => response.json())
+    .then(function(respuesta){
+        console.log("respuesta peticion", respuesta)
+    });
+    if(auth){
+        listarCategorias();
+    }
+    alertas("Se ha registrado la categoria exitosamente!",1)
+    //document.getElementById("contentModal").innerHTML = '';
+    var myModalEl = document.getElementById('modalUsuario')
+    var modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
+    modal.hide();
+}
+function eliminaCategoria(id){
+    validaToken();
+    var settings={
+        method: 'DELETE',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+    }
+    fetch(urlApi+"/categoria/"+id,settings)
+    .then((data) => {
+        console.log(data); // JSON data parsed by `data.json()` call
+        listarCategorias();
+        alertas("Se ha eliminado la categoria exitosamente!",2)
+      })
+}
+//-------------------------------------------------------------------------------------
+
+
+
+function verModificarCategoria(id){
+    validaToken();
+    var settings={
+        method: 'GET',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+    }
+    fetch(urlApi+"/categoria/id/"+id,settings)
+    .then(response => response.json())
+    .then(function(categoria){
+            var cadena='';
+            if(categoria){                
+                cadena = `
+                <div class="p-3 mb-2 bg-light text-dark">
+                    <h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Modificar categoria</h1>
+                </div>
+              
+                <form action="" method="post" id="myForm">
+
+
+
+                    <input type="hidden" name="id" id="id" value="${categoria.id}">
+                    <label for="nombre" class="form-label">Nombre</label>
+                    <input type="text" class="form-control" name="nombre" id="nombre" value="${categoria.nombre}" required> <br>
+                    
+                    <label for="descripcion"  class="form-label">Descripcion</label>
+                    <input type="text" class="form-control" name="descripcion" id="descripcion" value="${categoria.descripcion}" required> <br>
+                    
+
+                    <button type="button" class="btn btn-outline-warning" 
+                        onclick="modificarCategoria('${categoria.id}')">Modificar
+                    </button>
+                </form>`;
+            }
+            document.getElementById("contentModal").innerHTML = cadena;
+            var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
+            myModal.toggle();
+    })
+}
+
+async function modificarCategoria(id){
+    validaToken();
+    var myForm = document.getElementById("myForm");
+    var formData = new FormData(myForm);
+    var jsonData = {};
+    for(var [k, v] of formData){//convertimos los datos a json
+        jsonData[k] = v;
+    }
+    const request = await fetch(urlApi+"/categoria/"+id, {
+        method: 'PUT',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+        body: JSON.stringify(jsonData)
+    });
+    listarCategorias();
+    alertas("Se ha modificado la categoria exitosamente!",1)
+    document.getElementById("contentModal").innerHTML = '';
+    var myModalEl = document.getElementById('modalUsuario')
+    var modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
+    modal.hide();
+}
+// Modificar Categoria
+
+function verModificarCategoria(id){
+    validaToken();
+    var settings={
+        method: 'GET',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+    }
+    fetch(urlApi+"/categoria/id/"+id,settings)
+    .then(response => response.json())
+    .then(function(categoria){
+            var cadena='';
+            if(categoria){                
+                cadena = `
+                <div class="p-3 mb-2 bg-light text-dark">
+                    <h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Modificar Categoria</h1>
+                </div>
+              
+                <form action="" method="post" id="myForm">
+                <input type="hidden" name="id" id="id" value="${categoria.id}">
+                <label for="nombre" class="form-label">Nombre</label>
+                <input type="text" class="form-control" name="nombre" id="nombre" value="${categoria.nombre}" required> <br>
+                
+                <label for="descripcion"  class="form-label">Descripcion</label>
+                <input type="text" class="form-control" name="descripcion" id="descripcion" value="${categoria.descripcion}" required> <br>
+                
+                    <button type="button" class="btn btn-outline-warning" 
+                        onclick="modificarCategoria('${categoria.id}')">Modificar
+                    </button>
+                </form>`;
+            }
+            document.getElementById("registro").innerHTML = cadena;
+            var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
+            myModal.toggle();
+    })
+}
+
+async function modificarCategoria(id){
+    validaToken();
+    var myForm = document.getElementById("myForm");
+    var formData = new FormData(myForm);
+    var jsonData = {};
+    for(var [k, v] of formData){//convertimos los datos a json
+        jsonData[k] = v;
+    }
+ 
+    const request = await fetch(urlApi+"/categoria/"+id, {
+        method: 'PUT',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+        body: JSON.stringify(jsonData)
+    });
+    listarCategorias();
+    alertas("Se ha modificado la categoria exitosamente!",1)
+   // document.getElementById("contentModal").innerHTML = '';
+    var myModalEl = document.getElementById('modalUsuario')
+    var modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
+    modal.hide();
+}
+//Ver Categoria
+function verCategoria(id){
+    validaToken();
+    var settings={
+        method: 'GET',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.token
+        },
+    }
+    fetch(urlApi+"/categoria/id/"+id,settings)
+    .then(response => response.json())
+    .then(function(usuario){
+            var cadena='';
+            if(usuario){                
+                cadena = `
+                <div class="p-3 mb-2 bg-light text-dark">
+                    <h1 class="display-5"><i class="fa-solid fa-user-pen"></i> Ver Categoria</h1>
+                </div>
+              
+                <form action="" method="post" id="myForm">
+
+
+
+                    <input type="hidden" name="id" id="id" value="${usuario.id}">
+                    <label for="nombre" class="form-label">Nombre</label>
+                    <input type="text" class="form-control" name="nombre" id="nombre" value="${usuario.nombre}" required> <br>
+                    
+                    <label for="descripcion"  class="form-label">Descripcion</label>
+                    <input type="text" class="form-control" name="descripcion" id="descripcion" value="${usuario.descripcion}" required> <br>
+                   
+
+                    <button type="button" class="btn btn-outline-warning" data-bs-dismiss="modal" aria-label="Close">Cerrar</button>
+                </form>`;
+            }
+            document.getElementById("registro").innerHTML = cadena;
+            var myModal = new bootstrap.Modal(document.getElementById('modalUsuario'))
+            myModal.toggle();
+    })
+}
+
